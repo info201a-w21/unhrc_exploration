4974e29aa651f99a2f9391d710fc4734462a4341
Michael Freeman <michael.k.freeman@gmail.com>
2021-02-17 14:44:33 -0500

    Finish analysis v1

--- analysis.R
+++ analysis.R
@@ -4,6 +4,7 @@
 # Set up ------------------------------------------------------------------
 
 library(tidyverse)
+library(countrycode)
 all_data <- read.csv("data/population.csv", skip = 14)
 
 # Simple exploration
@@ -18,7 +19,8 @@ data <- all_data %>%
 dim(data)
 
 country_of_interest <- "ESP"
-
+country_name <- countrycode(country_of_interest, origin = 'iso3c', destination = 'country.name')
+  
 country_data <- data %>% 
   filter(Country.of.asylum..ISO. == country_of_interest)
 
@@ -42,3 +44,16 @@ top_10_countries <- country_data %>%
 # Make a map --------------------------------------------------------------
 
 shapefile <- map_data("world")
+# Get iso3 codes
+shapefile <- shapefile %>% 
+  mutate(Country.of.origin..ISO. = countrycode(region, origin = 'country.name', destination = 'iso3c')) %>% 
+  left_join(country_data, by = "Country.of.origin..ISO.")
+
+
+ggplot(data = shapefile) +
+  geom_polygon(
+    mapping = aes(x = long, y = lat, group = group, fill = Asylum.seekers)
+    ) +
+  labs(title = paste("Number of People Seeking Asylum in", country_name), 
+        x = "", y = "", fill = "Num. People") +
+  theme_minimal()
